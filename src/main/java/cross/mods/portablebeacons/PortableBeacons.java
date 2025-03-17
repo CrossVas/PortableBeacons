b35e49db3cb1583f5358f723c048bd04758413bc
CrossVas <cross.wanted.1.4@gmail.com>
2025-03-17 14:25:25 +0900

    removed unused entries

--- src/main/java/cross/mods/portablebeacons/PortableBeacons.java
+++ src/main/java/cross/mods/portablebeacons/PortableBeacons.java
@@ -4,13 +4,10 @@ import cross.mods.portablebeacons.init.PortableBeaconsTabs;
 import cross.mods.portablebeacons.init.PortableComponentData;
 import cross.mods.portablebeacons.init.PortableItems;
 import cross.mods.portablebeacons.utils.ConfigHandler;
-import cross.mods.portablebeacons.utils.CuriosLoader;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.ModConfigSpec;
@@ -29,19 +26,11 @@ public class PortableBeacons {
         CONFIG_SPEC = SPEC_PAIR.getRight();
     }
 
-    public static final CuriosLoader CURIO_LOADER = new CuriosLoader();
-    public static final boolean CURIO = ModList.get().isLoaded("curios");
-
     public PortableBeacons(IEventBus eventBus, ModContainer mod) {
         mod.registerConfig(ModConfig.Type.COMMON, CONFIG_SPEC);
         mod.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
         PortableBeaconsTabs.register(eventBus);
         PortableComponentData.register(eventBus);
         PortableItems.register(eventBus);
-        eventBus.addListener(this::commonLoad);
-    }
-
-    public void commonLoad(FMLCommonSetupEvent e) {
-        if (CURIO) CURIO_LOADER.preInit();
     }
 }
